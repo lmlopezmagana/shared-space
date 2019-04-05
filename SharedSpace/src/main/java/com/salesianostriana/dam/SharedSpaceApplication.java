01ff4053229da3b0b3620c56f58b861b0e8fb65b
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-04-05 13:18:16 +0200

    First Commit

---
+++ SharedSpace/src/main/java/com/salesianostriana/dam/SharedSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedSpaceApplication.class, args);
+	}
+
+}
